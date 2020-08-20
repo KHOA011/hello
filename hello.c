919c7a7d07d86a53833881621ed7a2863fb063de
Khoa Le <kle21@huskers.unl.edu>
2020-08-20 21:13:36 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+ /** 
+ * Author: Khoa Le
+ *         kle21@huskers.unl.edu
+ * Date: 2002/08/20
+ * 
+ * Simple program  
+ * 
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Khoa Le\n");
+     
+     return 0;
+ }
+ 
